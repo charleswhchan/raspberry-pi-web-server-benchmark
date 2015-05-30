7bc5046ca206216564c66bec74860a349aafc7cd
Charles Chan <charleswhchan@users.noreply.github.com>
2015-05-30 13:40:18 -0700

    Update hello.js

--- node.js/hello.js
+++ node.js/hello.js
@@ -1,10 +1,5 @@
 var http = require('http');
 
-# Disable request logging in console output
-import logging
-log = logging.getLogger('werkzeug')
-log.setLevel(logging.ERROR)
-
 http.createServer(function (req, res) {
   // res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
