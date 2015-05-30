269e9a0046bcb5984b8533d7f27db14cadd615f7
charleswhchan <charleswhchan@users.noreply.github.com>
2015-05-30 19:28:05 +0000

    CC - Node.js demo.

---
+++ node.js/hello.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+  // res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(1337, '0.0.0.0');
+
+console.log('Server running at http://0.0.0.0:1337/');
